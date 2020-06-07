fc753d2559039cd967cc5df3273d3cc5aecfee8d
Fashad-Ahmed <fashad.ahmed20@gmail.com>
2020-06-07 08:32:48 +0500

    latest commit

--- Basic JS/Functions.js
+++ Basic JS/Functions.js
@@ -2,9 +2,9 @@ function fun(name = 'the', lasttname = 'chainsmokers') {
     console.log( name + " hello "   + lasttname)
 }
 
-fun('alan','walker')
-fun('one','republic')
-fun( )
+fun('alan','walker');
+fun('one','republic');
+fun( );
 
 
 function square(num) {
@@ -12,5 +12,22 @@ function square(num) {
 }
 
 
-let num1 = square(3)
-console.log(num1)
+let num1 = square(3);
+console.log(num1);
+
+
+function greet(){
+    var a = prompt('hi enter yor name; ');
+    var b = "Hello my name is : " + a;
+    document.write(b);
+}
+
+greet();
+
+function hb(a1,a2){
+    var sum = a1 + a2;
+    console.log(sum);
+}
+
+hb(100,11);
+hb("Fashad ","Ahmed");
