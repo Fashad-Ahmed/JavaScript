30d29bff344cdbbbd0847eb78b8ff246ea27af97
Fashad Ahmed <fashad.ahmed20@gmail.com>
2020-07-01 14:54:56 +0500

    Add files via upload

---
+++ Basic JS/switch.js
@@ -0,0 +1,16 @@
+var name = "Puth"
+
+switch (name) {
+    case "Puth":
+        alert("Hello 1")
+        break;
+    case "mendes":
+        alert("Hello 2")
+        break;
+    default:
+        alert("hello")
+}
+
+
+
+// switch statement is fast then if-else
